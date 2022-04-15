b4cdcdd6f5c9fdcc489db4641ef0dab3e8c70fc2
sigmaducc <adnan.ch09@protonmail.com>
2022-04-15 23:28:14 +0530

    Tailwind setup configuration

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+    content: ["./index.html", "./src/**/*.{jsx,tsx}"],
+    theme: {
+        extend: {
+            screens: {
+                ssm: "500px",
+            },
+        },
+    },
+    plugins: [],
+};
