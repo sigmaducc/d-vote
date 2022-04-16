60aa1920a6194e05c03654c9a93cba0fe67bfd0a
sigmaducc <adnan.ch09@protonmail.com>
2022-04-17 03:35:56 +0530

    Made index file for images

---
+++ images/index.js
@@ -0,0 +1,16 @@
+export { default as metamask }
+from "./metamask.png";
+export { default as aadhar }
+from "./aadhar.png";
+export { default as pie }
+from "./pie.png";
+export { default as vote }
+from "./vote.png";
+export { default as logo28 }
+from "./logo28.png";
+export { default as logo36 }
+from "./logo36.png";
+export { default as logo48 }
+from "./logo48.png";
+export { default as logo72 }
+from "./logo72.png";
