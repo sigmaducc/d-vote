68b1b39c19ca449b631ac9ff4517f735978f51eb
sigmaducc <adnan.ch09@protonmail.com>
2022-04-17 03:28:00 +0530

    Index file with all the updated components

--- src/components/index.js
+++ src/components/index.js
@@ -1,6 +1,10 @@
 export { default as Navbar }
 from "./Navbar";
-export { default as MainSection }
-from "./MainSection";
+export { default as TimeLine }
+from "./TimeLine";
+export { default as Blockchain }
+from "./Blockchain";
+export { default as Democracy }
+from "./Democracy";
 export { default as Footer }
 from "./Footer";
