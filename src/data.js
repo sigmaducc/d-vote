f79aebc39657553a0b820f84231aedc5c97835a5
sigmaducc <adnan.ch09@protonmail.com>
2022-04-17 10:56:23 +0530

    Misc changes

---
+++ src/data.js
@@ -0,0 +1,28 @@
+const data = [{
+        id: 0,
+        description: "Every election is determined by the people who show up.",
+        spokesman: "Larry J. Sabato",
+    },
+    {
+        id: 1,
+        description: "Democracy is based upon the conviction that there are extraordinary possibilities in ordinary people.",
+        spokesman: "Harry Emerson Fosdick",
+    },
+    {
+        id: 2,
+        description: "There's no such thing as a vote that doesn't matter. It all matters.",
+        spokesman: "Barack Obama",
+    },
+    {
+        id: 3,
+        description: "The ballot is stronger than the bullet.",
+        spokesman: "Abraham Lincoln",
+    },
+    {
+        id: 4,
+        description: "Democracy is 'government of, by and for the people'.",
+        spokesman: "Abraham Lincoln",
+    },
+];
+
+export default data;
